dd565332bb275a5b437389f9fb6f5059f67fb4ef
DeveloperDemetri <developerdemetri@gmail.com>
2016-11-22 12:53:39 -0700

    working on exception handling

--- src/main/java/com/zoophy/ZooPhyController.java
+++ src/main/java/com/zoophy/ZooPhyController.java
@@ -3,10 +3,14 @@ package com.zoophy;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.zoophy.database.DaoException;
+import com.zoophy.database.GenBankRecordNotFoundException;
 import com.zoophy.database.ZoophyDAO;
 import com.zoophy.genbank.GenBankRecord;
 import com.zoophy.index.LuceneSearcher;
@@ -26,11 +30,15 @@ public class ZooPhyController {
 	
     /**
      * Retrieves the specified record from the database.
-     * @param accession - Accession of GenBankRecord to be retreived
+     * @param accession - Accession of GenBankRecord to be retrieved
+     * @param isFull - does the record need its associated Publication and Genes
      * @return specified record from the database.
+     * @throws GenBankRecordNotFoundException 
+     * @throws DaoException 
      */
     @RequestMapping("/record")
-    public GenBankRecord getDatabaseRecord(@RequestParam(value="accession") String accession, @RequestParam(value="isfull", required=false, defaultValue="true") Boolean isFull) {
+    @ResponseStatus(value=HttpStatus.OK)
+    public GenBankRecord getDatabaseRecord(@RequestParam(value="accession") String accession, @RequestParam(value="isfull", required=false, defaultValue="true") Boolean isFull) throws GenBankRecordNotFoundException, DaoException {
     	GenBankRecord gbr = null;
     	if (isFull) {
     		gbr = dao.retreiveFullRecord(accession);
@@ -47,6 +55,7 @@ public class ZooPhyController {
      * @return GenBankRecord results of given query.
      */
     @RequestMapping("/search")
+    @ResponseStatus(value=HttpStatus.OK)
     public List<GenBankRecord> queryLucene(@RequestParam(value="query") String query) {
     	List<GenBankRecord> results = indexSearcher.searchIndex(query);
     	return results;
--- src/main/java/com/zoophy/database/ZoophyDAO.java
+++ src/main/java/com/zoophy/database/ZoophyDAO.java
@@ -10,7 +10,7 @@ import com.zoophy.genbank.Gene;
 
 
 /**
- * Responsible for retreiving data from the SQL database.
+ * Responsible for retrieving data from the SQL database.
  * @author devdemetri
  */
 @Repository("ZoophyDAO")
@@ -20,43 +20,69 @@ public class ZoophyDAO {
     private JdbcTemplate jdbc;
 	
 	private static final String pullRecordDetails = "SELECT \"Sequence_Details\".\"Accession\", \"Collection_Date\", \"Comment\", \"Definition\", \"Isolate\", \"Tax_ID\", \"Organism\", \"Strain\", \"Sequence\", \"Segment_Length\", \"Host_Name\", \"Host_taxon\", \"Geoname_ID\", \"Location\", \"Latitude\", \"Longitude\", \"Type\" FROM \"Sequence_Details\" JOIN \"Host\" ON \"Sequence_Details\".\"Accession\"=\"Host\".\"Accession\" JOIN \"Location_Geoname\" ON \"Sequence_Details\".\"Accession\"=\"Location_Geoname\".\"Accession\" JOIN \"Sequence\" ON \"Sequence_Details\".\"Accession\"=\"Sequence\".\"Accession\" WHERE \"Sequence_Details\".\"Accession\"=?";
-	private static final String pullRecordGenes = "SELECT \"Gene_ID\", \"Accession\", \"Normalized_Gene_Name\" FROM \"Gene\" WHERE \"Accession\"=?";
+	private static final String pullRecordGenes = "SELECT \"Gene_ID\", \"Accession\", \"Normalized_Gene_Name\" FROM \"Gene\" WHERE \"Accession\"=?::text";
 	private static final String pullRecordPublication = "SELECT \"Accession\", \"Pubmed_ID\", \"Pubmed_Central_ID\", \"Authors\", \"Title\", \"Journal\" FROM \"Sequence_Publication\" JOIN \"Publication\" ON \"Sequence_Publication\".\"Pub_ID\"=\"Publication\".\"Pubmed_ID\" WHERE \"Accession\"=?";
 	
 	/**
 	 * Retrieve the specified GenBankRecord from the database without Gene or Publication details
 	 * @param accession - unique accession of record to be returned
 	 * @return - retrieved GenBankRecord from database
+	 * @throws GenBankRecordNotFoundException 
+	 * @throws DaoException 
 	 */
-	public GenBankRecord retreiveLightRecord(String accession) {
-		GenBankRecord record = null;
-		String[] param = {accession};
+	public GenBankRecord retreiveLightRecord(String accession) throws GenBankRecordNotFoundException, DaoException {
 		try {
-			record = jdbc.queryForObject(pullRecordDetails, param, new GenBankRecordRowMapper());
-		}
-		catch (EmptyResultDataAccessException erdae) {
+			GenBankRecord record = null;
+			String[] param = {accession};
+			try {
+				record = jdbc.queryForObject(pullRecordDetails, param, new GenBankRecordRowMapper());
+			}
+			catch (EmptyResultDataAccessException erdae) {
+				throw new GenBankRecordNotFoundException(accession);
+			}
 			return record;
 		}
-		return record;
+		catch (Exception e) {
+			if (e.getClass() != GenBankRecordNotFoundException.class) {
+				throw new DaoException(e.getMessage());
+			}
+			else {
+				throw e;
+			}
+		}
 	}
 	
 	/**
 	 * Retrieve the specified GenBankRecord from the database with all related details
 	 * @param accession - unique accession of record to be returned
 	 * @return - retrieved GenBankRecord from database
+	 * @throws GenBankRecordNotFoundException 
+	 * @throws DaoException 
 	 */
-	public GenBankRecord retreiveFullRecord(String accession) {
-		GenBankRecord record = null;
-		String[] param = {accession};
+	public GenBankRecord retreiveFullRecord(String accession) throws GenBankRecordNotFoundException, DaoException {
 		try {
-			record = jdbc.queryForObject(pullRecordDetails, param, new GenBankRecordRowMapper());
-//			record.setGenes(jdbc.queryForList(pullRecordGenes, Gene.class, param, new GeneRowMapper()));
-			record.setPub(jdbc.queryForObject(pullRecordPublication, param, new PublicationRowMapper()));
-		}
-		catch (EmptyResultDataAccessException erdae) {
+			GenBankRecord record = null;
+			String[] param = {accession};
+			try {
+				record = jdbc.queryForObject(pullRecordDetails, param, new GenBankRecordRowMapper());
+				//record.setGenes(jdbc.queryForList(pullRecordGenes, Gene.class, param, new GeneRowMapper()));
+				record.setPub(jdbc.queryForObject(pullRecordPublication, param, new PublicationRowMapper()));
+			}
+			catch (EmptyResultDataAccessException erdae) {
+				if (record == null) {
+					throw new GenBankRecordNotFoundException(accession);
+				}
+			}
 			return record;
 		}
-		return record;
+		catch (Exception e) {
+			if (e.getClass() != GenBankRecordNotFoundException.class) {
+				throw new DaoException(e.getMessage());
+			}
+			else {
+				throw e;
+			}
+		}
 	}
 	
 }
