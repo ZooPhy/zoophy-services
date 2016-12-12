bde70b9b91fd67bbe50ea4bcc693006833d4d5a7
DeveloperDemetri <developerdemetri@gmail.com>
2016-12-12 11:25:22 -0700

    adjusting lucene regex

--- src/main/java/edu/asu/zoophy/security/SecurityHelper.java
+++ src/main/java/edu/asu/zoophy/security/SecurityHelper.java
@@ -12,10 +12,10 @@ import org.springframework.stereotype.Repository;
 @Repository("SecurityHelper")
 public class SecurityHelper {
 	
-	private static final String ACCESSION_REGEX = "^([A-Z]|\\d|_|\\.){5,10}$";
-	private static final String LUCENE_REGEX = "^(\\w| |:|\\[|\\]|\\(|\\)){5,1024}$";
+	private static final String ACCESSION_REGEX = "^([A-Z]|\\d|_|\\.){5,10}+$";
+	private static final String LUCENE_REGEX = "^(\\w| |:|\\[|\\]|\\(|\\)){5,5000}+$";
 	private static final String EMAIL_REGEX = "^[-a-z0-9~!$%^&*_=+}{\\'?]+(\\.[-a-z0-9~!$%^&*_=+}{\\'?]+)*@([a-z0-9_][-a-z0-9_]*(\\.[-a-z0-9_]+[a-z][a-z])|([0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}))(:[0-9]{1,5})?$";
-	private static final String JOB_NAME_REGEX = "^(\\w| |-|_|#|&){3,255}$";
+	private static final String JOB_NAME_REGEX = "^(\\w| |-|_|#|&){3,255}+$";
 	
 	/**
 	 * Verifies parameters via regular expression
