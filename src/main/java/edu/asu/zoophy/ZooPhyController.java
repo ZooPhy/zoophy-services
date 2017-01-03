85f9b37e74cda4a85fa1fdee59ffa5048df301ef
DeveloperDemetri <developerdemetri@gmail.com>
2017-01-03 09:49:52 -0700

    logging all service requests

--- src/main/java/edu/asu/zoophy/ZooPhyController.java
+++ src/main/java/edu/asu/zoophy/ZooPhyController.java
@@ -5,6 +5,7 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -59,6 +60,8 @@ public class ZooPhyController {
 	@Autowired
 	private DownloadFormatter formatter;
 	
+	private static Logger log = Logger.getLogger("ZooPhyController");
+	
     /**
      * Retrieves the specified record from the database.
      * @param accession - Accession of GenBankRecord to be retrieved
@@ -74,14 +77,18 @@ public class ZooPhyController {
     	if (security.checkParameter(accession, Parameter.ACCESSION)) {
 	    	GenBankRecord record = null;
 	    	if (isFull) {
+	    		log.info("Retrieving full record: "+accession);
 	    		record = dao.retrieveFullRecord(accession);
 	    	}
 	    	else {
+	    		log.info("Retrieving light record: "+accession);
 	    		record = dao.retrieveLightRecord(accession);
 	    	}
+	    	log.info("Successfully retrieved record: "+accession);
 	    	return record;
     	}
     	else {
+    		log.warning("Bad record accession parameter: "+accession);
     		throw new ParameterException(accession);
     	}
     }
@@ -98,10 +105,13 @@ public class ZooPhyController {
     @ResponseStatus(value=HttpStatus.OK)
     public Location getRecordLocation(@RequestParam(value="accession") String accession) throws GenBankRecordNotFoundException, DaoException, ParameterException {
     	if (security.checkParameter(accession, Parameter.ACCESSION)) {
+    		log.info("Retrieving record location: "+accession);
     		Location location = dao.retrieveLocation(accession);
+    		log.info("Successfully retrieved location: "+accession);
     		return location;
     	}
     	else {
+    		log.warning("Bad accession parameter: "+accession);
     		throw new ParameterException(accession);
     	}
     }
@@ -118,10 +128,13 @@ public class ZooPhyController {
     @ResponseStatus(value=HttpStatus.OK)
     public List<GenBankRecord> queryLucene(@RequestParam(value="query") String query) throws LuceneSearcherException, InvalidLuceneQueryException, ParameterException {
     	if (security.checkParameter(query, Parameter.LUCENE_QUERY)) {
+    		log.info("Searching query: "+query);
     		List<GenBankRecord> results = indexSearcher.searchIndex(query);
+    		log.info("Successfully searched query: "+query);
     		return results;
     	}
     	else {
+    		log.warning("Bad query parameter: "+query);
     		throw new ParameterException(query);
     	}
     }
@@ -138,8 +151,10 @@ public class ZooPhyController {
     @ResponseStatus(value=HttpStatus.OK)
     public List<GenBankRecord> queryAccessions(@RequestBody List<String> accessions) throws ParameterException, LuceneSearcherException, InvalidLuceneQueryException {
     	List<GenBankRecord> records = null;
+    	log.info("Searching accession list...");
     	if (accessions != null && accessions.size() > 0) {
     		if (accessions.size() > 1023) {
+    			log.warning("Accession list is too long.");
     			throw new ParameterException("accessions list is too long");
     		}
     		Set<String> uniqueAccessions = new LinkedHashSet<String>(accessions.size());
@@ -148,6 +163,7 @@ public class ZooPhyController {
     				uniqueAccessions.add(accession);
     			}
     			else {
+    				log.warning("Bad accession parameter: "+accession);
     				throw new ParameterException(accession);
     			}
     		}
@@ -164,6 +180,11 @@ public class ZooPhyController {
     		queryBuilder.append(")");
     		// TODO: Need to use a different method to allow over 1024 accessions down the road. 
     		records = indexSearcher.searchIndex(queryBuilder.toString());
+    		log.info("Successfully searched accession list.");
+    	}
+    	else {
+    		log.warning("Accession list is empty.");
+    		records = new LinkedList<GenBankRecord>();
     	}
     	return records;
     }
@@ -178,6 +199,7 @@ public class ZooPhyController {
     @RequestMapping(value="/run", method=RequestMethod.POST, headers="Accept=application/json")
     @ResponseStatus(value=HttpStatus.ACCEPTED)
     public String runZooPhyJob(@RequestBody JobParameters parameters) throws ParameterException, PipelineException {
+    	log.info("Starting new job...");
     	if (security.checkParameter(parameters.getReplyEmail(), Parameter.EMAIL)) {
     		ZooPhyRunner zoophy;
 	    	if (parameters.getJobName() == null) {
@@ -188,6 +210,7 @@ public class ZooPhyController {
 	    			zoophy = new ZooPhyRunner(parameters.getReplyEmail(), parameters.getJobName());
 	    		}
 	    		else {
+	    			log.warning("Bad job name parameter: "+parameters.getJobName());
 	    			throw new ParameterException(parameters.getJobName());
 	    		}
 	    	}
@@ -197,16 +220,20 @@ public class ZooPhyController {
 	    			jobAccessions.add(accession);
 	    		}
 	    		else {
+	    			log.warning("Bad accession parameter: "+accession);
 	    			throw new ParameterException(accession);
 	    		}
 	    	}
 	    	if (jobAccessions.size() > JOB_MAX_ACCESSIONS) {
+	    		log.warning("Job accession list is too long.");
 	    		throw new ParameterException("accessions list is too long");
 	    	}
 	    	manager.startZooPhyPipeline(zoophy, new ArrayList<String>(jobAccessions));
+	    	log.info("Job successfully started: "+zoophy.getJobID());
 	    	return zoophy.getJobID();
     	}
     	else {
+    		log.warning("Bad reply email parameter: "+parameters.getReplyEmail());
     		throw new ParameterException(parameters.getReplyEmail());
     	}
     }
@@ -221,15 +248,19 @@ public class ZooPhyController {
     @ResponseStatus(value=HttpStatus.OK)
     public String stopZooPhyJob(@RequestParam(value="id") String jobID) throws PipelineException, ParameterException {
     	if (security.checkParameter(jobID, Parameter.JOB_ID)) {
+    		log.info("Stopping ZooPhy Job: "+jobID);
     		try {
 	    		manager.killJob(jobID);
+	    		log.info("Successfully stopped ZooPhy Job: "+jobID);
 	    		return "Successfully stopped ZooPhy Job: "+jobID;
     		}
     		catch (PipelineException pe) {
+    			log.warning("Failed to stop job: "+jobID+" : "+pe.getMessage());
     			throw new ParameterException(jobID);
     		}
     	}
     	else {
+    		log.warning("Bad Job ID parameter: "+jobID);
     		throw new ParameterException(jobID);
     	}
     }
@@ -245,11 +276,14 @@ public class ZooPhyController {
     @RequestMapping(value="/download", method=RequestMethod.POST)
     @ResponseStatus(value=HttpStatus.OK)
     public String retrieveDownload(@RequestParam(value="format") String format, @RequestBody List<String> accessions) throws ParameterException, FormatterException {
+    	log.info("Setting up download...");
     	if (format != null) {
     		if (accessions == null || accessions.size() == 0) {
+    			log.warning("Empty accession list.");
     			return null;
     		}
     		if (accessions.size() > 2500) {
+    			log.warning("Too many accessions.");
     			throw new ParameterException("accessions list is too long");
     		}
     		Set<String> downloadAccessions = new LinkedHashSet<String>(accessions.size());
@@ -258,22 +292,30 @@ public class ZooPhyController {
     				downloadAccessions.add(accession);
 	    		}
 	    		else {
+	    			log.warning("Bad accession parameter: "+accession);
 	    			throw new ParameterException(accession);
 	    		} 
     		}
     		accessions = new LinkedList<String>(downloadAccessions);
     		downloadAccessions.clear();
+    		String download = null;
     		if (format.equalsIgnoreCase("CSV")) {
-    			return formatter.generateDownload(accessions, DownloadFormat.CSV);
+    			log.info("Generating CSV download...");
+    			download = formatter.generateDownload(accessions, DownloadFormat.CSV);
     		}
     		else if (format.equalsIgnoreCase("FASTA")) {
-    			return formatter.generateDownload(accessions, DownloadFormat.FASTA);
+    			log.info("Generating FASTA download...");
+    			download = formatter.generateDownload(accessions, DownloadFormat.FASTA);
     		}
     		else {
+    			log.warning("Bad format parameter: "+format);
     			throw new ParameterException(format);
     		}
+    		log.info("Successfully generated download.");
+    		return download;
     	}
     	else {
+    		log.warning("Bad format parameter: "+format);
     		throw new ParameterException(format);
     	}
     }
