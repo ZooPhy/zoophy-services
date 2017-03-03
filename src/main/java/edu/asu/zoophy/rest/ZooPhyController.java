ad702adb0cad37d2a8ea30048224a4ba52e71e4d
DeveloperDemetri <developerdemetri@gmail.com>
2017-03-03 12:00:15 -0700

    changed how errors are sent for validation service

--- src/main/java/edu/asu/zoophy/rest/ZooPhyController.java
+++ src/main/java/edu/asu/zoophy/rest/ZooPhyController.java
@@ -394,50 +394,75 @@ public class ZooPhyController {
     /**
      * Validate a ZooPhy Job before starting it, to avoid common failures in the early stages.
      * @param parameters
-     * @return True if the job passes the sensitive early stages
+     * @return Null if the job is valid, otherwise the String of Error message(s).
      * @throws ParameterException
      * @throws PipelineException
      */
     @RequestMapping(value="/validate", method=RequestMethod.POST, headers="Accept=application/json")
     @ResponseStatus(value=HttpStatus.OK)
-    public boolean validateJob(@RequestBody JobParameters parameters) throws ParameterException, PipelineException {
+    public String validateJob(@RequestBody JobParameters parameters) throws ParameterException, PipelineException {
     	log.info("Validating job...");
-    	if (security.checkParameter(parameters.getReplyEmail(), Parameter.EMAIL)) {
-    		ZooPhyRunner zoophy;
-	    	if (parameters.getJobName() != null) {
-	    		if (!security.checkParameter(parameters.getJobName(), Parameter.JOB_NAME)) {
-	    			log.warning("Bad job name parameter: "+parameters.getJobName());
-	    			throw new ParameterException(parameters.getJobName());
+    	try {
+	    	if (security.checkParameter(parameters.getReplyEmail(), Parameter.EMAIL)) {
+	    		ZooPhyRunner zoophy;
+		    	if (parameters.getJobName() != null) {
+		    		if (!security.checkParameter(parameters.getJobName(), Parameter.JOB_NAME)) {
+		    			log.warning("Bad job name parameter: "+parameters.getJobName());
+		    			throw new ParameterException(parameters.getJobName());
+		    		}
+		    	}
+	    		try {
+	    			security.verifyXMLOptions(parameters.getXmlOptions());
 	    		}
-	    	}
-    		try {
-    			security.verifyXMLOptions(parameters.getXmlOptions());
-    		}
-    		catch (ParameterException pe) {
-    			log.warning("Bad XML Parameters: "+pe.getMessage());
-    			throw pe;
-    		}
-	    	zoophy = new ZooPhyRunner(parameters.getReplyEmail(), parameters.getJobName(), parameters.isUsingGLM(), parameters.getPredictors());
-	    	Set<String> jobAccessions = new LinkedHashSet<String>(parameters.getAccessions().size());
-	    	for(String accession : parameters.getAccessions()) {
-	    		if  (security.checkParameter(accession, Parameter.ACCESSION)) {
-	    			jobAccessions.add(accession);
+	    		catch (ParameterException pe) {
+	    			log.warning("Bad XML Parameters: "+pe.getMessage());
+	    			throw pe;
 	    		}
-	    		else {
-	    			log.warning("Bad accession parameter: "+accession);
-	    			throw new ParameterException(accession);
-	    		}
+		    	zoophy = new ZooPhyRunner(parameters.getReplyEmail(), parameters.getJobName(), parameters.isUsingGLM(), parameters.getPredictors());
+		    	Set<String> jobAccessions = new LinkedHashSet<String>(parameters.getAccessions().size());
+		    	for(String accession : parameters.getAccessions()) {
+		    		if  (security.checkParameter(accession, Parameter.ACCESSION)) {
+		    			jobAccessions.add(accession);
+		    		}
+		    		else {
+		    			log.warning("Bad accession parameter: "+accession);
+		    			throw new ParameterException(accession);
+		    		}
+		    	}
+		    	if (jobAccessions.size() > JOB_MAX_ACCESSIONS) {
+		    		log.warning("Job accession list is too long.");
+		    		throw new ParameterException("accessions list is too long");
+		    	}
+		    	zoophy.testZooPhy(new ArrayList<String>(jobAccessions), dao, indexSearcher);
+		    	return null;
 	    	}
-	    	if (jobAccessions.size() > JOB_MAX_ACCESSIONS) {
-	    		log.warning("Job accession list is too long.");
-	    		throw new ParameterException("accessions list is too long");
+	    	else {
+	    		log.warning("Bad reply email parameter: "+parameters.getReplyEmail());
+	    		throw new ParameterException(parameters.getReplyEmail());
 	    	}
-	    	zoophy.testZooPhy(new ArrayList<String>(jobAccessions), dao, indexSearcher);
-	    	return true;
     	}
-    	else {
-    		log.warning("Bad reply email parameter: "+parameters.getReplyEmail());
-    		throw new ParameterException(parameters.getReplyEmail());
+    	catch (ParameterException pe) {
+    		return pe.getMessage();
+    	}
+    	catch (GLMException glme) {
+    		if (glme.getUserMessage() != null) {
+    			return glme.getUserMessage();
+    		}
+    		else {
+    			return "GLM Tools Failed";
+    		}
+    	}
+    	catch (PipelineException pe) {
+    		if (pe.getUserMessage() != null) {
+    			return pe.getUserMessage();
+    		}
+    		else {
+    			return "ZooPhy Pipeline Failed";
+    		}
+    	}
+    	catch (Exception e) {
+    		log.warning("Unknown Pipeline error occurred: "+e.getMessage());;
+    		return "Unkown Error occurred";
     	}
     }
     
